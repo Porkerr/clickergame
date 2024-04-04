3a27f7ee9135bb1cdb4dbf5767934358d23a56c0
cuppsos <163440510+cuppsos@users.noreply.github.com>
2024-04-04 13:15:55 -0400

    Update Circle.py

--- Circle.py
+++ Circle.py
@@ -1,12 +1,5 @@
 import pygame
-"""
-Circle Class is NOT FINISHED
-     At the moment I have not found a way to implement one function to render the
-circle so we can calculate with it and one to draw the circle. This leads to some
-strange code in the running method of the Game class where the circle is drawn so
-we can calculate with it and then drawn once again later after we fill the screen black.
-Not sure if there is a workaround for this but I will leave it for one of you guys
-"""
+# Circle class
 class Circle:
     # draw_circle method requires self, radius, color ([x, y, z]), position (x, y), and returns it
     def draw_circle(self, radius, color = [], position = []):
