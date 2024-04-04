8640591f62b6b30ff800b6bc106cd6eca6fa4434
cuppsos <163440510+cuppsos@users.noreply.github.com>
2024-04-04 13:13:45 -0400

    Update game.py
    At the moment I have not found a way to implement one function to render the circle so we can calculate with it and one to draw the circle. 
    This leads to some strange code in the running method of the Game class where the circle is drawn so we can calculate with it and then drawn once again later after we fill the screen black.
    My current workaround for this is to draw the first circle with color values [0, 0, 0] and draw the second with the correct color values.
    I think there is a way to simplify this but I have made lots of changes to the code and I want you guys to look at them before we proceed.

--- game.py
+++ game.py
@@ -1,50 +1,38 @@
 import pygame
 import sys
+from Circle import Circle
+from Rectangle import Rectangle
 
 class Game:
     def __init__(self):
         pygame.init()  # Initialize pygame
-
+        
         # Set up the display window
         pygame.display.set_caption("Clicker Game")  # Sets the window title !
         self.screen = pygame.display.set_mode((800, 600))  # sets the aspect ratio
         self.clock = pygame.time.Clock()  # clock for controlling frame rate
         
-        # Circle properties
-        self.circle_radius = 200  # radius of  circle
-        self.circle_color = (0, 255, 0)  #  color of circle
-        self.circle_position = (self.circle_radius, 375)  # Positions circle on  left side of screen
-
-        # Upgrade buttons (rectangles) properties
-        self.rect1_width = 300  # width of the rectangle1
-        self.rect1_height = 100  # height of the rectangle1
-        self.rect1_color = (0, 0, 255)  # blue color for the top rectangle1
-        self.rect1_position = (450, 50)  # pos for the top rectangle1
-        
-        self.rect2_width = 300  # width of the rectangle2
-        self.rect2_height = 100  # height of the rectangle2
-        self.rect2_color = (0,120,120)  # green color for the bottom rectangle2
-        self.rect2_position = (450, 200)  # pos for the bottom rectangle2
-
-        # Times Clicked tracker
-        self.rect3_width = 300  # width of the rectangle3
-        self.rect3_height = 100  # height of the rectangle3
-        self.rect3_color = (0, 125, 125) # color for rectangle3
-        self.rect3_position = (50, 50) # pos for rectangle3
-
         # the font for our text, can change later if u feelin fancy
         self.font = pygame.font.Font(None, 36)  # Set font for text rendering
-
+        
         # text for when u click circle
         self.circle_text = self.font.render("Click me for points", True, (255, 255, 255))  # This is gonna render our 'click me' text for circle
-        self.circle_text_rect = self.circle_text.get_rect(center=self.circle_position)  # tHis positions the text within the circle's center
-
+        
         # text for upgrade buttons(they're rectangles so i used rect)
         self.rect1_text = self.font.render("Upgrade", True, (255, 255, 255))  # Render text for rectangle1
         self.rect2_text = self.font.render("Upgrade", True, (255, 255, 255))  # Render text for rectangle2
-
+        
     def running(self):
+        # Render rects for Upgrade buttons
+        rect1 = Rectangle.rend_rect(300, 100, [450, 200])
+        rect2 = Rectangle.rend_rect(300, 100, [450, 400])
+        # Render rect for counter
+        rect3 = Rectangle.rend_rect(300, 100, [75, 50])    
+        # Draw circle for calculating properties
+        circle = Circle.draw_circle(self, 200, [0, 0, 0], [225, 375])
+        
         times_clicked = 0 # Number of times circle clicked
+
         while True:
             for event in pygame.event.get():  # Check for events
                 if event.type == pygame.QUIT:  # if quit event is detected makes ya leave the game
@@ -53,8 +41,8 @@ class Game:
                 elif event.type == pygame.MOUSEBUTTONDOWN:  # THIS is important. It basically can detect whenever the mouse button is pressed
                     mouse_x, mouse_y = pygame.mouse.get_pos()  # Get mouse position. We need this so we know what's being clicked.
                     # Check if mouse click occurred within the circle
-                    #For checking if a mouse is over something, I figured out you can use the distance formula (Euclidean distance).. some complex math thing you probably dont want me to explain bc you're tired of math classes
-                    if ((mouse_x - self.circle_position[0])**2 + (mouse_y - self.circle_position[1])**2) <= self.circle_radius**2:
+                    # For checking if a mouse is over something, I figured out you can use the distance formula (Euclidean distance).. some complex math thing you probably dont want me to explain bc you're tired of math classes
+                    if ((mouse_x - circle.centerx)**2 + (mouse_y - circle.centery)**2) <= (circle.width/2)**2:
                         times_clicked+=1 # Increment times circle is clicked
                         print("Circle clicked!")  # For demonstration purposes
                         print("Times clicked: " + str(times_clicked)) # For demonstration purposes
@@ -62,38 +50,38 @@ class Game:
                     # Check if mouse click occurred within the top upgrade button
                     #For checking if the rectangles are clicked, our code directly compares the mouse coordinates (mouse_x, mouse_y)                      
                     #with the bounding box defined by the positions and dimensions of the rectangles.                                          
-                    if (self.rect1_position[0] <= mouse_x <= self.rect1_position[0] + self.rect1_width) and \
-                       (self.rect1_position[1] <= mouse_y <= self.rect1_position[1] + self.rect1_height):
+                    if (rect1.x <= mouse_x <= rect1.x + rect1.width) and \
+                       (rect1.y <= mouse_y <= rect1.y + rect1.height):
                         print("Rectangle 1 clicked!")
 
                     # Check if mouse click occurred within the bottom rectangle button
-                    if (self.rect2_position[0] <= mouse_x <= self.rect2_position[0] + self.rect2_width) and \
-                       (self.rect2_position[1] <= mouse_y <= self.rect2_position[1] + self.rect2_height):
+                    if (rect2.x <= mouse_x <= rect2.x + rect2.width) and \
+                       (rect2.y <= mouse_y <= rect2.y + rect2.height):
                         print("Rectangle 2 clicked!")
-
-            # Fill the screen  
+ 
+            # Fill the screen
             self.screen.fill("Black")
             
             # Draws the circle on the screen
-            pygame.draw.circle(self.screen, self.circle_color, self.circle_position, self.circle_radius)
+            Circle.draw_circle(self, 200, [0, 255, 0], [225, 375])
+
+            # Draw the upgrade buttons on the screen
+            Rectangle.draw_rect(self, (0, 0, 255), rect1)
+            Rectangle.draw_rect(self, (125, 125, 12), rect2)
             
-            # Draw the rectangles (upgrade buttons) on the screen
-            pygame.draw.rect(self.screen, self.rect1_color, (self.rect1_position, (self.rect1_width, self.rect1_height)))
-            pygame.draw.rect(self.screen, self.rect2_color, (self.rect2_position, (self.rect2_width, self.rect2_height)))
-            pygame.draw.rect(self.screen, self.rect3_color, (self.rect3_position, (self.rect3_width, self.rect3_height)))
+            # Draw the counter on the screen
+            Rectangle.draw_rect(self, (200, 40, 60), rect3)
             
             # render/display text for the circle
-            self.screen.blit(self.circle_text, self.circle_text_rect)
+            # tHis positions the text within the circle's center
+            self.screen.blit(self.circle_text, self.circle_text.get_rect(center=circle.center))
             
-            #  render/display text for the rectangles
-            self.rect3_text = self.font.render("Times Clicked: " + str(times_clicked), True, (255, 255, 255)) # Render text for rectangle3
-            
-            self.screen.blit(self.rect1_text, (self.rect1_position[0] + self.rect1_width/2 - self.rect1_text.get_width()/2, 
-                                              self.rect1_position[1] + self.rect1_height/2 - self.rect1_text.get_height()/2))
-            self.screen.blit(self.rect2_text, (self.rect2_position[0] + self.rect2_width/2 - self.rect2_text.get_width()/2, 
-                                              self.rect2_position[1] + self.rect2_height/2 - self.rect2_text.get_height()/2))
-            self.screen.blit(self.rect3_text, (self.rect3_position[0] + self.rect3_width/2 - self.rect3_text.get_width()/2, 
-                                              self.rect3_position[1] + self.rect3_height/2 - self.rect3_text.get_height()/2))
+            # render/display text for the rectangles
+            # rect3_text must be rendered each time the text is drawn to screen  
+            self.screen.blit(self.rect1_text, Rectangle.center_rect_text(self, rect1, self.rect1_text))
+            self.screen.blit(self.rect2_text, Rectangle.center_rect_text(self, rect2, self.rect2_text))
+            self.rect3_text = self.font.render("Times Clicked: " + str(times_clicked), True, (255, 255, 255))
+            self.screen.blit(self.rect3_text, Rectangle.center_rect_text(self, rect3, self.rect3_text))
 
             self.clock.tick(60)  # Limits frame rate to 60 FPS
             
